f5cc65d3c4f1bd0dc2b4e8c77b819540ba79641e
sebprt <sebastien.parrat@orange.fr>
2021-05-31 14:48:11 +0200

    Added missing Expression type

--- src/CompositeBuilder.php
+++ src/CompositeBuilder.php
@@ -105,7 +105,7 @@ final class CompositeBuilder implements \IteratorAggregate, CompositeBuilderInte
         return $this;
     }
 
-    public function list(string $outputPath, string $expression): ArrayBuilderInterface
+    public function list(string $outputPath, string|Expression $expression): ArrayBuilderInterface
     {
         $child = new ArrayBuilder($this, $this->interpreter);
 
@@ -121,7 +121,7 @@ final class CompositeBuilder implements \IteratorAggregate, CompositeBuilderInte
         return $child;
     }
 
-    public function map(string $outputPath, string $expression): ArrayBuilderInterface
+    public function map(string $outputPath, string|Expression $expression): ArrayBuilderInterface
     {
         $child = new ArrayBuilder($this, $this->interpreter);
 
@@ -135,7 +135,7 @@ final class CompositeBuilder implements \IteratorAggregate, CompositeBuilderInte
         return $child;
     }
 
-    public function object(string $outputPath, string $className, string $expression): ObjectBuilderInterface
+    public function object(string $outputPath, string $className, string|Expression $expression): ObjectBuilderInterface
     {
         $child = new ObjectBuilder($className, $this, $this->interpreter);
 
@@ -151,7 +151,7 @@ final class CompositeBuilder implements \IteratorAggregate, CompositeBuilderInte
         return $child;
     }
 
-    public function collection(string $outputPath, string $className, string $expression): ObjectBuilderInterface
+    public function collection(string $outputPath, string $className, string|Expression $expression): ObjectBuilderInterface
     {
         $child = new ObjectBuilder($className, $this, $this->interpreter);
 
